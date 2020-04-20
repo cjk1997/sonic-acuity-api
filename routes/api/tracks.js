6be7e5934965dd7c07f756505d424cd7bdd08f5c
Carson Keller <carsonjkeller@gmail.com>
2020-04-20 15:43:59 -0600

    Created and implemented POST router

--- routes/api/tracks.js
+++ routes/api/tracks.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const getTracks = require('../../data/getTracks');
-// const addTrack = require('../../data/addTrack');
+const addTrack = require('../../data/addTrack');
 // const updateTrack = require('../../data/updateTrack');
 // const deleteTrack = require('../../data/deleteTrack')
 
@@ -12,6 +12,20 @@ router.get('/', async function(req, res, next) {
     } catch(err) {
         console.log(err);
         res.send(500, "Internal Server Issue, check logs");
+    };
+});
+
+router.post('/', async function (req, res, next) {
+    try {
+        const track = await addTrack(req.body);
+        res.send(track);
+    } catch(err) {
+        if (err.error) {
+            res.status(400).send(err);
+        } else {
+            console.log(err);
+            res.status(500).send("Internal Sever Issue, check logs")
+        };
     };
 });
 
