1e831b4eefba8e17960a76902ed0160b3869ffcb
Carson Keller <carsonjkeller@gmail.com>
2020-04-20 10:46:19 -0600

    Fixed router implementation

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const createError = require('http-errors');
-const cors = require('cors')
+const cors = require('cors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
@@ -16,12 +16,12 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors())
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
