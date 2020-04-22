00648d0bda1e2921160642f1113dc458e29abb57
Carson Keller <carsonjkeller@gmail.com>
2020-04-22 08:31:51 -0600

    Updated router name

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const tracksRouter = require('./routes/api/tracks');
+const tracksAPIRouter = require('./routes/api/tracks');
 
 const app = express();
 
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api/tracks', tracksRouter);
+app.use('/api/tracks', tracksAPIRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
