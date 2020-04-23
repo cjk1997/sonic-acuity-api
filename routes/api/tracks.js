1940949e0b8e47437bc61196bae3115075a69f1b
Carson Keller <carsonjkeller@gmail.com>
2020-04-23 14:03:39 -0600

    Added router for PUT functionality

--- routes/api/tracks.js
+++ routes/api/tracks.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const getTracks = require('../../data/getTracks');
 const addTrack = require('../../data/addTrack');
-// const updateTrack = require('../../data/updateTrack');
+const updateTrack = require('../../data/updateTrack');
 const deleteTrack = require('../../data/deleteTrack');
 
 router.get('/', async function(req, res, next) {
@@ -26,6 +26,16 @@ router.post('/', async function (req, res, next) {
             console.log(err);
             res.status(500).send("Internal Sever Issue, check logs");
         };
+    };
+});
+
+router.put('/:id', async function (req, res) {
+    try {
+        const data = await updateTrack(req.params.id, req.body);
+        res.send(data);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send ("Internal Server Issue, chack logs")
     };
 });
 
